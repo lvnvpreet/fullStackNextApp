f45280016bfd013c8ef5d0258c3aa5fee805e632
lvnvpreet <dhillonlovepreet147@gmail.com>
2024-12-19 13:18:04 +0530

    Add email verification functionality with Resend integration

---
+++ src/lib/resend.ts
@@ -0,0 +1,5 @@
+import { Resend } from 'resend';
+
+
+eport const resend = new Resend(process.env.RESEND_API_KEY);
+
---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,9 @@
+import { Message } from "@/model/user.model";
+
+export interface ApiResponse {
+    success: boolean;
+    message: string;
+    isAccesptingMessages?: boolean;
+    messages?: Array<Message>;
+
+}
